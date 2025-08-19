bc13d24b46b893a9e6bd41ed9ded151c4e6698ba
imkmss <signkms14@gmail.com>
2025-08-19 16:08:42 +0900

    Revert "명서 로컬 전체 저장 [ Search Rooms by Keywords fuction 까지 반영]"
    This reverts commit 880f7ee5467304ce507d202191b25c46105404a6.

--- src/main/java/com/example/zabang_be/repository/RoomRepository.java
+++ src/main/java/com/example/zabang_be/repository/RoomRepository.java
@@ -7,6 +7,4 @@ import java.util.List;
 public interface RoomRepository extends JpaRepository<RoomEntity, Long> {
     // 빌라, 원룸을 이름으로 검색할 수 있는 메서드 선언 (포함하는 값도 검색된다 SQL의 Like)
     List<RoomEntity> findRoomEntitiesByNameContaining(String name);
-    // 키워드는 지정되어 있기 때문에 굳이 포함 값은 하지 않음
-    List<RoomEntity> findRoomEntitiesByKeywords(List<String> keywords);
 }
