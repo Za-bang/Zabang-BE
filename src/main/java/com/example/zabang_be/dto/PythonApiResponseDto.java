7c8633607220a4d544e80468e7543828ca069eab
kimdh2002 <kdh101701@gmail.com>
2025-08-25 01:29:24 +0900

    docs : KeywordByAiService

--- src/main/java/com/example/zabang_be/dto/PythonApiResponseDto.java
+++ src/main/java/com/example/zabang_be/dto/PythonApiResponseDto.java
@@ -1,10 +1,13 @@
 package com.example.zabang_be.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PythonApiResponseDto {
-    @JsonProperty("keywords")
     private String keywords;
 }
